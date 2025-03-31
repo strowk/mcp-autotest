f20a9f65ecb7dee7283789715f1935b6a831cc0f
Timur Sultanaev <str.write@gmail.com>
2025-03-31 21:45:52 +0200

    fix publish npm

--- packages/publish_npm.sh
+++ packages/publish_npm.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-# set -e
+set -e
 
-# # # Mac
-# mkdir -p ./packages/npm-mcp-autotest-darwin-x64/bin
-# cp dist/mcp-autotest_darwin_amd64_v1/mcp-autotest ./packages/npm-mcp-autotest-darwin-x64/bin/mcpt
-# chmod +x ./packages/npm-mcp-autotest-darwin-x64/bin/mcpt
-# mkdir -p ./packages/npm-mcp-autotest-darwin-arm64/bin
-# cp dist/mcp-autotest_darwin_arm64_v8.0/mcp-autotest ./packages/npm-mcp-autotest-darwin-arm64/bin/mcpt
-# chmod +x ./packages/npm-mcp-autotest-darwin-arm64/bin/mcpt
+# # Mac
+mkdir -p ./packages/npm-mcp-autotest-darwin-x64/bin
+cp dist/mcp-autotest_darwin_amd64_v1/mcp-autotest ./packages/npm-mcp-autotest-darwin-x64/bin/mcpt
+chmod +x ./packages/npm-mcp-autotest-darwin-x64/bin/mcpt
+mkdir -p ./packages/npm-mcp-autotest-darwin-arm64/bin
+cp dist/mcp-autotest_darwin_arm64_v8.0/mcp-autotest ./packages/npm-mcp-autotest-darwin-arm64/bin/mcpt
+chmod +x ./packages/npm-mcp-autotest-darwin-arm64/bin/mcpt
 
-# # # Linux
-# mkdir -p ./packages/npm-mcp-autotest-linux-x64/bin
-# cp dist/mcp-autotest_linux_amd64_v1/mcp-autotest ./packages/npm-mcp-autotest-linux-x64/bin/mcpt
-# chmod +x ./packages/npm-mcp-autotest-linux-x64/bin/mcpt
-# mkdir -p ./packages/npm-mcp-autotest-linux-arm64/bin
-# cp dist/mcp-autotest_linux_arm64_v8.0/mcp-autotest ./packages/npm-mcp-autotest-linux-arm64/bin/mcpt
-# chmod +x ./packages/npm-mcp-autotest-linux-arm64/bin/mcpt
+# # Linux
+mkdir -p ./packages/npm-mcp-autotest-linux-x64/bin
+cp dist/mcp-autotest_linux_amd64_v1/mcp-autotest ./packages/npm-mcp-autotest-linux-x64/bin/mcpt
+chmod +x ./packages/npm-mcp-autotest-linux-x64/bin/mcpt
+mkdir -p ./packages/npm-mcp-autotest-linux-arm64/bin
+cp dist/mcp-autotest_linux_arm64_v8.0/mcp-autotest ./packages/npm-mcp-autotest-linux-arm64/bin/mcpt
+chmod +x ./packages/npm-mcp-autotest-linux-arm64/bin/mcpt
 
-# # # Windows
-# mkdir -p ./packages/npm-mcp-autotest-win32-x64/bin
-# cp dist/mcp-autotest_windows_amd64_v1/mcp-autotest.exe ./packages/npm-mcp-autotest-win32-x64/bin/mcpt.exe
-# mkdir -p ./packages/npm-mcp-autotest-win32-arm64/bin
-# cp dist/mcp-autotest_windows_arm64_v8.0/mcp-autotest.exe ./packages/npm-mcp-autotest-win32-arm64/bin/mcpt.exe
+# # Windows
+mkdir -p ./packages/npm-mcp-autotest-win32-x64/bin
+cp dist/mcp-autotest_windows_amd64_v1/mcp-autotest.exe ./packages/npm-mcp-autotest-win32-x64/bin/mcpt.exe
+mkdir -p ./packages/npm-mcp-autotest-win32-arm64/bin
+cp dist/mcp-autotest_windows_arm64_v8.0/mcp-autotest.exe ./packages/npm-mcp-autotest-win32-arm64/bin/mcpt.exe
 
 cd packages/npm-mcp-autotest-darwin-x64
-# npm publish --access public
+npm publish --access public
 
-# cd ../npm-mcp-autotest-darwin-arm64
-# npm publish --access public
+cd ../npm-mcp-autotest-darwin-arm64
+npm publish --access public
 
-# cd ../npm-mcp-autotest-linux-x64
-# npm publish --access public
+cd ../npm-mcp-autotest-linux-x64
+npm publish --access public
 
-# cd ../npm-mcp-autotest-linux-arm64
-# npm publish --access public
+cd ../npm-mcp-autotest-linux-arm64
+npm publish --access public
 
-# cd ../npm-mcp-autotest-win32-x64
-# npm publish --access public
+cd ../npm-mcp-autotest-win32-x64
+npm publish --access public
 
-# cd ../npm-mcp-autotest-win32-arm64
-# npm publish --access public
+cd ../npm-mcp-autotest-win32-arm64
+npm publish --access public
 
 cd ../npm-mcp-autotest
 npm publish --access public
